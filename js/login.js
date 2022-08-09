7ef5743038ddf0c71fc5d413c6a176c908f063a5
Nicolás <108936163+nicolasamoroso@users.noreply.github.com>
2022-08-09 15:56:02 -0300

    google oauth + login fix

--- js/login.js
+++ js/login.js
@@ -1,3 +1,34 @@
-function handleCredentialResponse() {
-  window.location.href = "inicio.html"
+//google oauth
+
+function handleCredentialResponse(response) {
+  const responsePayload = decodeJwtResponse(response.credential);
+  window.location.href = "inicio.html?name=" + responsePayload.name
 }
+
+function decodeJwtResponse (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+};
+
+
+//validation email and password
+
+(function () {
+  'use strict'
+  let forms = document.querySelectorAll('.needs-validation')
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
